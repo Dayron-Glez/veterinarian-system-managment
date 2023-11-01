1447f6180279ea0c0b82533f18743a811c0bf5fa
Dayron-Glez <dayronglez01@gmail.com>
2023-10-31 21:22:07 -0400

    CREATED: Form validations

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,14 +1,50 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+
+
 const LoginComponent = () => {
   const navigate = useNavigate();
+  const {register,handleSubmit,formState:{errors}} = useForm()
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  })
   return (
     <>  
         <main className='main-container'>
-          <form action="submit" className='form-container'>
-              <input placeholder='Input your username' type="text" name="name" id="name" />
-              <input placeholder='Input your password' type="password" name="password" id="password" />
-              <button onClick={() => navigate('/ReceptionComponent')} type='submit'>Submit</button>
+          <form onSubmit={onSubmit} className='form-container'>
+              <input placeholder='Input your username' type="text" name="name" id="name" {...register('username', {
+                required: {
+                  value: true,
+                  message: 'Username is required'
+                },
+                minLength: {
+                  value:3,
+                  message: 'Minimun 3 characters in the username field'
+                },
+                maxLength:{
+                  value:30,
+                  message: 'Maximum 30 characters int the username field'
+                }
+              })}  autoComplete='off'/>
+
+               
+              {errors.username && <span>{errors.username.message}</span>}
+
+              <input placeholder='Input your password' type="password" name="password" id="password" {...register('password', {
+                required:{
+                  value:true,
+                  message: 'Password is required',
+                },
+                pattern: {
+                  value:/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                  message: 'Wrong password format'
+                }
+              })}  autoComplete='off'/>
+
+              {errors.password && <span>{errors.password.message}</span>}
+
+              <button type='submit'>Submit</button>
           </form>
         </main>
     </>
--- src/components/ReceptionComponent.jsx
+++ src/components/ReceptionComponent.jsx
@@ -1,22 +1,84 @@
-import React from 'react'
+import { useForm } from 'react-hook-form';
 import { useNavigate } from "react-router-dom";
-
 const ReceptionComponent = () => {
   const navigate = useNavigate();
+  const {register,handleSubmit,formState:{errors}} = useForm()
 
+  const onSubmit = handleSubmit((value) => {
+    console.log(value);
+  })
+  
   return (
-   <form action="submit" >
+   <form onSubmit={onSubmit} className='client-pet-form'>
     <h2>Client</h2>
-        <input type="text" name="name" id="name" />
-        <input type="text" name="DNI" id="DNI" />
-        <input type="text" name="phone" id="phone" />
+        <input placeholder='Input client name' type="text"  name="client_name" id="client_name"  {...register('client_name', {
+          required: {
+            value:true,
+            message: 'client name is required'
+          },
+          minLength: {
+            value:3,
+            message: 'Minimun 3 characters in the client name field'
+          },
+          maxLength:{
+            value:30,
+            message: 'Maximum 30 characters int the client name field'
+          },
+          autoComplete: {
+            value: 'off'
+          }
+        })}/>
+
+        {errors.client_name && <span>{errors.client_name.message}</span>}
+
+        <input placeholder='Input client DNI'   type="text" name="DNI" id="DNI"  {...register('DNI',{
+          required: {
+            value:true,
+            message: 'client DNI is required'
+          },
+          autoComplete:'off'
+        })} />
+
+        {errors.DNI && <span>{errors.DNI.message}</span>}
+
+        <input placeholder='Input client phone' type="text" name="phone" id="phone"  {...register('phone',{
+          required: {
+            value:true,
+            message: 'The phone number is required'
+          }
+        })}/>
+
+        {errors.phone && <span>{errors.phone.message}</span>}
 
     <h2>Pet</h2>
-        <input type="text" name="name" id="name" />
-        <input type="text" name="race" id="race" />
-        <input type="number" name="weight" id="weight"/>
-        <input type="text" name="fur" id="fur" />
-        <button onClick={() => navigate('/DoctorComponent')} type="submit">Add</button>
+        <input placeholder='Input pet name' type="text"     name="pet_name" id="pet_name"   {...register('pet_name',{
+          required: {
+            value: true,
+            message: 'Pet name is required'
+          }
+        })}/>
+        {errors.pet_name && <span>{errors.pet_name.message}</span>}
+        
+        <input placeholder='Input pet race' type="text"     name="race" id="race"   {...register('race',{
+          autoComplete: {
+            value: 'off'
+          }
+        })} />
+        <input placeholder='Input pet weight' type="number" name="weight" id="weight"  {...register('weight',{
+          required: {
+            value: true,
+            message: "The pet's weight is required"
+          }
+        })}/>
+
+        {errors.weight && <span>{errors.weight.message}</span>}
+
+        <input placeholder='Input pet fur' type="text"      name="fur" id="fur" {...register('fur',{
+          autoComplete: {
+            value: 'off'
+          }
+        })} />
+        <button type="submit">Add</button>
    </form>
   )
 }
