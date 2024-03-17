0652c16048e31281d272e083ebd14533826f3e8b
Dayron-Glez <dayronglez01@gmail.com>
2024-03-17 01:25:40 -0400

    FINISHED: ECOP8

--- src/components/ECOP/TerapeuticComponent.jsx
+++ src/components/ECOP/TerapeuticComponent.jsx
@@ -7,6 +7,24 @@ import Select from 'react-select'
 export const TerapeuticComponent = ({ register, control, index, ...props }) => {
     // eslint-disable-next-line no-unused-vars
     const [tipoTratamiento, setTipoTratamiento] = useState('')
+    const customStyles = {
+        control: (provided) => ({
+          ...provided,
+          width: '8rem', // w-32
+          height: '2rem', // h-8
+          border: 'none', // border-none
+          outline: 'none', // outline-1
+          '&:focus': {
+            outline: '#eb5b27', // focus:outline-[#eb5b27]
+            borderRadius: '0.25rem' // focus:rounded-sm
+          },
+        }),
+        
+        placeholder: (provided) => ({
+          ...provided,
+          paddingLeft: '1rem', // placeholder:pl-4
+        }),
+      };
     const options = [
         {
             name: '1',
@@ -60,8 +78,8 @@ export const TerapeuticComponent = ({ register, control, index, ...props }) => {
                             isSearchable
                             isClearable
                             noOptionsMessage={() => 'No existe esa opción'}
-                            className=' placeholder:font-bold placeholder:pl-4 w-32  h-8 border-none  outline-1 focus:outline-[#eb5b27]  focus:rounded-sm'
                             placeholder='Ej. Text'
+                            styles={customStyles}
                         />
                     )}
                 />
@@ -70,13 +88,13 @@ export const TerapeuticComponent = ({ register, control, index, ...props }) => {
 
 
 
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-36  ml-5 h-8 border-none  outline-1 focus:outline-[#eb5b27]  focus:rounded-sm' placeholder='Ej. Text' {...register(`principio_activo${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[120px] ml-6  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`presentacion${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-24 ml-5  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`posologia${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[98px] ml-5  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`dosis_total${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-28 ml-4  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`via${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[100px] ml-4  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`frecuencia${index}`)} />
-                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-32 ml-10  h-8 border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`duracion${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-36  ml-5 h-[38px] border-none  outline-1 focus:outline-[#eb5b27]  focus:rounded-sm' placeholder='Ej. Text' {...register(`principio_activo${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[120px] ml-6  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`presentacion${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-24 ml-5  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`posologia${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[98px] ml-5  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`dosis_total${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-28 ml-4  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`via${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-[100px] ml-4  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`frecuencia${index}`)} />
+                <input type="text" className=' placeholder:font-bold placeholder:pl-4 w-32 ml-10  h-[38px] border-none  outline-1  focus:outline-[#eb5b27] focus:rounded-sm' placeholder='Ej. Text' {...register(`duracion${index}`)} />
             </div>
             <div className=' border-[1px] border-solid border-[#b5b7ba] h-0  w-[100%]' />
 
